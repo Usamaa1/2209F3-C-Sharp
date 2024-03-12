4048d1c9ba6e8dac159c67236489f3e3366f1152
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-03-12 16:25:08 +0500

    OOP's Completed

--- 2209F3-Lecture1/2209F3-Lecture1/Program.cs
+++ 2209F3-Lecture1/2209F3-Lecture1/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 /*Console.WriteLine("Hello, World!");
 Console.WriteLine("This is John\"s Pen");
 
@@ -31,8 +31,7 @@ Console.WriteLine(pi);
 Console.WriteLine($"Hello {b} :-{{");
 */
 
-using System.Globalization;
-using System.Xml.Linq;
+
 /*
 string name = "Usama";
 
@@ -314,28 +313,28 @@ foreach (int number in numbers)
 //}
 
 
-List<int> nums = new List<int>();
+//List<int> nums = new List<int>();
 
-nums.Add(1);
-nums.Add(2);
-nums.Add(14);
-nums.Add(6);
-nums.Add(27);
-nums.Add(8);
-nums.Add(19);
-
-
-
-Console.WriteLine("before sorting");
+//nums.Add(1);
+//nums.Add(2);
+//nums.Add(14);
+//nums.Add(6);
+//nums.Add(27);
+//nums.Add(8);
+//nums.Add(19);
 
 
-nums.Reverse();
+
+//Console.WriteLine("before sorting");
 
 
-foreach (int i in nums)
-{
-    Console.WriteLine(i);
-}
+//nums.Reverse();
+
+
+//foreach (int i in nums)
+//{
+//    Console.WriteLine(i);
+//}
 
 //Console.WriteLine(nums[3]);
 
@@ -345,43 +344,321 @@ foreach (int i in nums)
 //Console.WriteLine("Index of 8: "+nums.IndexOf(8));
 
 //nums.Insert(5, 2000);
-nums.Sort();
+//nums.Sort();
 
 //Console.WriteLine($"Total Count of list is: {nums.Count}");
 //Console.WriteLine($"Last Index: {nums.LastIndexOf(19)}");
 //Console.WriteLine($"Contains: {nums.Contains(19)}");
 //Console.WriteLine($"Sort: ");
 
-nums.Reverse();
+//nums.Reverse();
 
-Console.WriteLine("After sorting");
-//nums.Clear();
-foreach (int i in nums)
+//Console.WriteLine("After sorting");
+////nums.Clear();
+//foreach (int i in nums)
+//{
+//    Console.WriteLine("nums list: " + i);
+//}
+
+
+//int[] numbers = nums.ToArray();
+
+//Console.WriteLine("numbers: "+numbers);
+//Console.WriteLine("list: "+nums);
+
+//foreach(int n in numbers)
+//{
+//    Console.WriteLine("numbers array: "+n);
+//}
+
+
+using gamespace;
+
+
+//Game GTA = new Game();
+//Game superMario = new Game();
+
+//GTA.name = "GTA 7";
+//GTA.company = "Rockstar";
+//GTA.releaseDate = "2027";
+//GTA.rating = 4.5;
+
+//Console.WriteLine(GTA.name);
+
+
+//superMario.name = "Super Mario Bros. Wonder";
+//superMario.company = "Nintendo";
+//superMario.releaseDate = "2023";
+//superMario.rating = 4.7;
+
+
+//Console.WriteLine($"Super Mario Company: {superMario.company}");
+
+
+
+
+//Game supermario = new()
+//{
+//    name = "Super Mario 7",
+//    company = "Nintendo",
+//    releaseDate = "02-03-2025",
+//    rating = 4.0,
+//};
+
+
+//Console.WriteLine(supermario.name);
+//Console.WriteLine(supermario.company);
+
+
+//Game supermario = new Game("Super Mario 7", "Nintendo", "02-03-2025", 4.5);
+
+//Game GTA = new Game("GTA 7");
+
+//supermario.name = "Assassin Creed";
+
+//Game CoD = new Game(gameRating:3.4, gameCompany:"Activision", gameReleaseDate:"03-05-2025", gameName:"Call of Duty 6");
+
+
+//Console.WriteLine(supermario.name);
+//Console.WriteLine(GTA.name);
+//Console.WriteLine("The Game Name :{0} and the company is:{1} and its release Date:{2} and its rating is: {3}",
+//    CoD.name,CoD.company,CoD.releaseDate,CoD.rating);
+//Console.WriteLine($"The Game Name :{CoD.name} and the company is:{CoD.company} and its " +
+//    $"release Date:{CoD.releaseDate} and its rating is: {CoD.rating}");
+//Console.WriteLine(supermario.company);
+
+
+//Game hobo = new Game("Hobo 5", "Fully", "09-07-2024", 5.0);
+
+//Console.WriteLine(hobo.name);
+//Console.WriteLine(hobo.rating);
+
+//supermario.isGameAvailable("nohjhsadfjh");
+
+
+//Game.isWebsiteDown("fjdskfj");
+
+Nintendo supermario = new()
 {
-    Console.WriteLine("nums list: " + i);
-}
+    gameName = "Super mario 7",
+    company = "Nintendo.org"
+};
+
+Console.WriteLine(supermario.gameName);
+
+supermario.gameDesc();
 
 
-int[] numbers = nums.ToArray();
-
-Console.WriteLine("numbers: "+numbers);
-Console.WriteLine("list: "+nums);
-
-foreach(int n in numbers)
+Ubisoft farCry = new()
 {
-    Console.WriteLine("numbers array: "+n);
-}
+    gameName = "Far Cry 3",
+    gameVersion = "4.5",
+    company = "Ubisoft"
+
+};
+
+farCry.gameDesc();
+
+Console.WriteLine(farCry.ToString());
 
 
+Nintendo cod = new()
+{
+    gameName = "Call of Duty 7",
+   
 
+};
 
+cod.setUserLoginId("73824875892347785");
 
+Console.WriteLine(cod.printUserLoginId());
 
-
-
-
-
+//Console.WriteLine(cod.uPassword());
 
 
 
 Console.ReadKey();
+
+namespace gamespace
+{
+    //class Game
+    //{
+    //    public string name { get; set; }
+    //    public string company;
+    //    public string releaseDate;
+    //    public double rating;
+
+
+
+    //    public Game(string gameName, string gameCompany, string gameReleaseDate, double gameRating)
+    //    {
+    //        this.name = gameName;
+    //        this.company = gameCompany;
+    //        this.releaseDate = gameReleaseDate;
+    //        this.rating = gameRating;
+
+    //    }
+
+    //    public Game(string gameName, string gameCompany, string gameReleaseDate)
+    //    {
+    //        this.name = gameName;
+    //        this.company = gameCompany;
+    //        this.releaseDate = gameReleaseDate;
+
+    //    }
+    //    public Game(string gameName, string gameCompany)
+    //    {
+    //        this.name = gameName;
+    //        this.company = gameCompany;
+    //    }
+    //    public Game(string gameName)
+    //    {
+    //        this.name = gameName;
+
+    //    }
+
+
+    //    public void isGameAvailable(string value)
+    //    {
+    //        if(value == "yes")
+    //        {
+    //            Console.WriteLine("The game is available");
+    //        }
+    //        else if(value == "no")
+    //        {
+    //            Console.WriteLine("The game is not available");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("You have entered wrong value");
+    //        }
+    //    }
+
+
+    //    public static void isWebsiteDown(string value)
+    //    {
+    //        if (value == "yes")
+    //        {
+    //            Console.WriteLine("The website is down due to some issues");
+    //        }
+    //        else if (value == "no")
+    //        {
+    //            Console.WriteLine("The website is available download your fav. games");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("You have entered wrong value");
+    //        }
+    //    }
+
+
+
+
+
+
+    //}
+
+
+
+
+
+
+
+
+
+    abstract class Game
+    {
+        public string gameName;
+        public string gameVersion;
+        public string company;
+        private string userPassword;
+        protected string userLoginId;
+
+
+        public virtual void gameDesc() { }
+
+        public void passwordReset(string newPassword){
+            this.userPassword = newPassword;
+        }
+        //public string uPassword()
+        //{
+        //    return this.userPassword;
+        //}
+
+    }
+
+
+    class Nintendo : Game
+    {
+        
+        public override void gameDesc()
+        {
+            Console.WriteLine($"Game name {this.gameName} and Company name {this.company}");
+        }
+
+        public void setUserLoginId(string userId)
+        {
+            this.userLoginId = userId;
+        }
+        public string printUserLoginId()
+        {
+            return this.userLoginId;
+        }
+
+    }
+
+
+    class Ubisoft : Game
+    {
+
+        public override void gameDesc()
+        {
+            Console.WriteLine($"Game name {this.gameName} and Company name {this.company} and its version is {this.gameVersion}");
+            base.gameDesc();
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Game Name: {this.gameName} \n Company Name: {this.company} \n Game Version: {this.gameVersion} \n User Login Id: {this.userLoginId} ";
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
