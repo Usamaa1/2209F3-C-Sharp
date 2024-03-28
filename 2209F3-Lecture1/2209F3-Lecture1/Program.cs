a6f3710a2f62ca775e2b794486f774c420a2461a
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-03-28 15:10:14 +0500

    IEnumerables and IEnumerators

--- 2209F3-Lecture1/2209F3-Lecture1/Program.cs
+++ 2209F3-Lecture1/2209F3-Lecture1/Program.cs
@@ -748,6 +748,495 @@ class HudaBeauty : IElectronics, ICosmetics
 
 
 
+//Console.WriteLine((int)weekDays.Thursday);
+
+
+
+/*if(weekDays.Thursday == weekDays.Monday)
+{
+    Console.WriteLine("Equal");
+}
+else
+{
+    Console.WriteLine("Not Equal");
+}
+*/
+
+/*var input = weekDays.Monday;
+
+if (input == weekDays.Thursday)
+{
+    Console.WriteLine("Today is Thursday");
+}
+else if(input == weekDays.Monday)
+{
+    Console.WriteLine("Today is Monday");
+
+}
+else
+{
+    Console.WriteLine("Not Equal");
+}
+*/
+
+
+/*var input = weekDays.Tuesday;
+
+switch (input)
+{
+    case weekDays.Monday:
+        Console.WriteLine("Today is Monday");
+        break;
+    case weekDays.Tuesday:
+        Console.WriteLine("Today is Tuesday");
+        break;
+    case weekDays.Wednesday:
+        Console.WriteLine("Today is wednesday");
+        break;
+    case weekDays.Thursday:
+        Console.WriteLine("Today is Thursday");
+        break;
+    default:
+        Console.WriteLine("");
+        break;
+}*/
+
+
+
+
+
+
+
+
+
+/*enum weekDays
+{
+    Monday = 90,
+    Tuesday = 80,
+    Wednesday = 78,
+    Thursday = 44
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*Console.WriteLine("The area of planet is: " + 4 * Math.PI * Math.Pow(((int)planets.Earth),2));
+
+
+Console.WriteLine(Math.PI);
+
+
+
+
+
+
+enum planets
+{
+    Jupiter = 69911,
+    Mars = 3389,
+    Earth = 6371,
+    Pluto = 1151
+}
+*/
+
+
+
+/*
+string[] names = ["usama", "arif","Zahid", "Hanif", "Aslam"];
+int[] nums = [1, 2, 34, 5, 7, 33, 43];
+
+products.printAll(names);
+products.printAll(nums);
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+products.add(454);
+
+//products.add1("fdsjfkjsda");
+
+
+
+
+
+class products
+{
+    public static void printAll<T>(T[] items)
+    {
+        foreach(T item in items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public static void add<A>(A a)
+    {
+        Console.WriteLine(a);
+    }
+    public static void add1(int a)
+    {
+        Console.WriteLine(a);
+    }
+}*/
+
+
+
+
+/*Products headphone = new();
+
+*//*headphone[0] = 3;
+headphone[0] = 5;*//*
+headphone[0] = 3;
+headphone[1] = 2.5;
+headphone[2] = 3.2;
+headphone[3] = 3.4;
+headphone[4] = 5.0;
+headphone[5] = 6;
+
+//Console.WriteLine(headphone[0]);
+
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(headphone[i]);
+}
+
+
+
+
+
+
+class Products
+{
+    private double[] rating = new double[10];
+
+    public double this[int index]
+    {
+        get 
+        {
+            return rating[index];
+        }
+        set 
+        {
+            if(value > 0 && value <= 5)
+            {
+                rating[index] = value;
+            }
+            else
+            {
+                Console.WriteLine("Enter values above from 0 and below from 5.1");
+            }
+        }
+    }
+
+}
+*/
+
+
+
+//Timer.timer1();
+//Timer.timer2();
+//Timer.timer3();
+
+/*
+Thread t1 = new Thread(Timer.timer1);
+Thread t2 = new Thread(Timer.timer2);
+Thread t3 = new Thread(Timer.timer3);
+
+t1.Start();
+t2.Start();
+t3.Start();
+
+
+
+class Timer
+{
+    public static void timer1 ()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{i} second of timer 1");
+            Thread.Sleep( 1000 );
+        }
+        Console.WriteLine("Timer 1 completed");
+    }  
+    public static void timer2 ()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"{i} second of timer 2");
+
+            Thread.Sleep( 2000 );
+        }
+        Console.WriteLine("Timer 2 completed");
+
+    }
+    public static void timer3 ()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"{i} second of timer 3");
+            Thread.Sleep( 3000 );
+        }
+        Console.WriteLine("Timer 3 completed");
+
+    }
+}
+*/
+
+
+
+
+
+
+
+
+//Timer.timer1();
+//Timer.timer2();
+//Timer.timer3();
+
+
+
+
+//await Task.WhenAll(Timer.timer1(), Timer.timer2(), Timer.timer3());
+
+
+/*await Timer.timer3();
+await Timer.timer1();
+
+
+
+
+
+class Timer
+{
+    public static async Task timer1()
+    {
+        await Task.Run(() => {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{i} second of timer 1");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Timer 1 completed");
+        });
+       
+    }
+    public static async Task timer2()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"{i} second of timer 2");
+
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine("Timer 2 completed");
+        });
+       
+
+    }
+    public static async Task timer3()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"{i} second of timer 3");
+                Thread.Sleep(3000);
+            }
+            Console.WriteLine("Timer 3 completed");
+        });
+       
+
+    }
+}
+*/
+
+/*using System.Net;
+
+Console.WriteLine("Enter your File Name: ");
+string fileName = Console.ReadLine();
+Console.WriteLine("Enter your web Address: ");
+string webAddress = Console.ReadLine();
+
+Scrapper.textToFile(fileName, webAddress);
+
+
+
+class Scrapper
+{
+    public static void textToFile(string fileName, string address)
+    {
+        WebClient client = new WebClient();
+        string reply = client.DownloadString(address);
+
+        Console.WriteLine(reply);
+        // Create a string with a line of text.
+        string text = reply;
+
+        // Set a variable to the Documents path.
+        string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        // Write the text to a new file named "WriteFile.txt".
+        string fullPath = Path.Combine(docPath, "Scrapper");
+
+
+
+        File.WriteAllText(Path.Combine(fullPath, fileName), text);
+
+        Console.WriteLine("Text Written successfully!");
+    }
+
+
+
+
+}
+*/
+
+
+
+/*Person p1 = new("Zahid", "Khan", 19);
+
+Console.WriteLine(p1.firstName);
+Console.WriteLine(p1);
+
+
+public record Person(string firstName, string lastName, int age);
+*/
+
+
+/*
+List<Person> persons = new List<Person>() {
+    new()
+{
+    firstName = "Zahid",
+    lastName = "Khan",
+    age = 30,
+},
+     new()
+{
+    firstName = "Hina",
+    lastName = "Younus",
+    age = 24,
+},
+     new()
+{
+    firstName = "Haris",
+    lastName = "Aslam",
+    age = 28,
+},
+     new()
+{
+    firstName = "Owais",
+    lastName = "Ahmed",
+    age = 58,
+}
+
+};
+*/
+/*
+Console.WriteLine(persons);*/
+
+
+
+//var above18 =
+//    from d in persons
+//    where d.age < 30
+//    orderby d.age descending
+//    select d;
+
+
+//var above18 = persons.Where(d => d.age < 30);
+
+
+//foreach (var a in above18)
+//{
+//    Console.WriteLine(a);
+//}
+
+
+
+
+/*foreach (Person person in persons)
+{
+    Console.WriteLine(person);
+}*/
+
+
+/*Person p1 = new()
+{
+    firstName = "Zahid",
+    lastName = "Khan",
+    age = 30,
+};
+Person p2 = new()
+{
+    firstName = "Ashar",
+    lastName = "Imtiaz",
+    age = 34,
+};
+*/
+/*Console.WriteLine(p1);
+Console.WriteLine(p2);*/
+//public record Person
+//{
+//    public string firstName;
+//    public string lastName;
+//    public int age;
+//}
+
+
+
+/*using System.Collections;
+
+Products headphone = new();
+headphone[0] = 3.4;
+headphone[1] = 2.4;
+headphone[2] = 4.4;
+headphone[3] = 5.4;
+
+foreach (var item in headphone)
+{
+    Console.WriteLine(item);
+}
+
+
+public class Products : IEnumerable
+{
+    public double[] ratings = new double[4];
+
+    public double this[int index]
+    {
+        get { return ratings[index]; }
+        set { ratings[index] = value; }
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return ratings.GetEnumerator();
+    }
+}
+*/
+
 
 
 
